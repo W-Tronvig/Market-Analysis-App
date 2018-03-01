74247ced91020ada00c9df865562609b45c49b68
William Tronvig <william.tronvig@gmail.com>
2018-02-28 18:47:56 -0800

    constructor function set up with array

--- JS/app.js
+++ JS/app.js
@@ -1,45 +1,39 @@
-"use strict";
+// 'use strict';
 
 //object literal 
 
-const catalog = {
-    products: [],
-    display: function (){
-        this.products.push(
-            new product('r2d2 bag', 'IMG/bag.jpg'),
-            new product('banana slicer', 'IMG/banana.jpg'),
-            new product('toeless boots', 'IMG/boots.jpg'),
-            new product('odd chair', 'IMG/chair.jpg'),
-            new product('creature', 'IMG/cthulhu.jpg'),
-            new product('dragon spam', 'IMG/dragon.jpg'),
-            new product('pentencil', 'IMG/pen.jpg'),
-            new product('pizza scissors', 'IMG/scissors.jpg'),
-            new product('shark sleeping bag', 'IMG/shark.jpg'),
-            new product('baby broom', 'IMG/sweep.jpg'),
-            new product('unicorn spam', 'IMG/unicorn.jpg'),
-            new product('usb alien', 'IMG/usb.jpg'),
-            new product('repouring can', 'IMG/water_can.jpg'),
-            new product('undrinkable wine glass', 'IMG/wine_glass.jpg')
-        );
-        this.displayProducts();
-    },
-    displayProducts: function(){
-        var randomProduct = Math.random();
-        // grab three images at random
-        // display images side by side on page
-        this.products[];
-    }
-};
-
 //constructor function
 
-var Catalog = function(productName, productMeta, productClicks, productRank){ //properties of catalog object
-    this.productName = productName;
-    this.productMeta = productMeta;
-    this.productClicks = productClicks;
-    this.productRank = productRank;
-    // I think this will be where the main functionality of the page goes. Basically I would want the following to occur:
-    // - Once image becomes clicked, then three new images appear. Page logs which image was clicked on. 
-}
+// var Catalog = function(productName, productMeta, productClicks, productRank){ //properties of catalog object
+//     this.productName = productName;
+//     this.productMeta = productMeta;
+//     this.productClicks = productClicks;
+//     this.productRank = productRank;
+
+    // - Upon page load, have three images appear on the screen in accordance with HTML and CSS applied. 
+    // - Display text above the images to let the user know to click on the image.
+    // - Once image becomes clicked, then three new images appear and browser keeps track of clicks without reloading page completely.
+    // - Store images in an array or maybe an array within an array? Have a loop so that the randomizer can randomly select images from the loop and display three at a time. 
+    // - Have function that displays images. 
+    // - Have function that displays totals, in a table of some type.
+    // - Try for function that displays totals in different types of ways, percentages, data points on table. // 
 
 // catalog.display();
+
+
+// this.productsArray.push(
+//     new Product('r2d2 bag', 'IMG/bag.jpg'),
+//     new Product('banana slicer', 'IMG/banana.jpg'),
+//     new Product('toeless boots', 'IMG/boots.jpg'),
+//     new Product('odd chair', 'IMG/chair.jpg'),
+//     new Product('creature', 'IMG/cthulhu.jpg'),
+//     new Product('dragon spam', 'IMG/dragon.jpg'),
+//     new Product('pentencil', 'IMG/pen.jpg'),
+//     new Product('pizza scissors', 'IMG/scissors.jpg'),
+//     new Product('shark sleeping bag', 'IMG/shark.jpg'),
+//     new Product('baby broom', 'IMG/sweep.jpg'),
+//     new Product('unicorn spam', 'IMG/unicorn.jpg'),
+//     new Product('usb alien', 'IMG/usb.jpg'),
+//     new Product('repouring can', 'IMG/water_can.jpg'),
+//     new Product('undrinkable wine glass', 'IMG/wine_glass.jpg')
+//     );
---
+++ JS/newapp.js
@@ -0,0 +1,32 @@
+'use strict';
+// Product Constructor
+
+var Product = function (name, url) {
+    this.name = name;
+    this.url = url;
+    this.clicks = 0;
+};
+
+
+var randomProductDisplay = function (){
+    
+}
+
+
+var productsArray = [];
+productsArray.push(
+    new Product('r2d2 bag', 'IMG/bag.jpg'),
+    new Product('banana slicer', 'IMG/banana.jpg'),
+    new Product('toeless boots', 'IMG/boots.jpg'),
+    new Product('odd chair', 'IMG/chair.jpg'),
+    new Product('creature', 'IMG/cthulhu.jpg'),
+    new Product('dragon spam', 'IMG/dragon.jpg'),
+    new Product('pentencil', 'IMG/pen.jpg'),
+    new Product('pizza scissors', 'IMG/scissors.jpg'),
+    new Product('shark sleeping bag', 'IMG/shark.jpg'),
+    new Product('baby broom', 'IMG/sweep.jpg'),
+    new Product('unicorn spam', 'IMG/unicorn.jpg'),
+    new Product('usb alien', 'IMG/usb.jpg'),
+    new Product('repouring can', 'IMG/water_can.jpg'),
+    new Product('undrinkable wine glass', 'IMG/wine_glass.jpg')
+);
